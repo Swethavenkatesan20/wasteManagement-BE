c815900f4e4f1e7ca7dac09c3bb92af078813541
Swethavenkatesan20 <157340837+Swethavenkatesan20@users.noreply.github.com>
2024-05-23 00:51:42 +0530

    Update app.js
    updated frontend deployed url

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const app=express();
 
 //enable cors request
 app.use(cors({
-    origin: 'http://localhost:5173',  // allow all origin
+    origin: 'https://waste-management-fe.vercel.app',  // allow all origin
     credentials: true
 }));
 
